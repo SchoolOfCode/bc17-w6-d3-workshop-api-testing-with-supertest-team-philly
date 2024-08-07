1937c8866b24608c8ab6b9215249e75b16705ef8
Abhiparth Shrestha <abhiparth@hotmail.co.uk>
2024-08-07 15:34:17 +0100

    created postgres db

---
+++ users/users.test.js
@@ -0,0 +1,8 @@
+// Your next task is to create a users.test.js file within the users folder. Then within 
+// the users.test.js file, write a bare bones test (the test name can be "GET /api/health works" 
+//     and the test callback can be left empty). Whilst this 
+// isn't a useful test yet, it's enough to register the test. Then run Vitest to check it detects the test file and the test passes.
+
+test("GET /api/health works", () => {
+
+});
